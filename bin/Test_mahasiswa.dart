5dbdd804619a9256cc8a36bdbded6d49f7113b01
nova3101 <novacantikafitri@gmail.com>
2021-04-19 10:16:01 +0700

    first commit

---
+++ bin/Test_mahasiswa.dart
@@ -0,0 +1,5 @@
+import 'package:semester4/semester4.dart' as semester4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${semester4.calculate()}!');
+}
---
+++ test/semester4_test.dart
@@ -0,0 +1,8 @@
+import 'package:semester4/mahasiswa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
